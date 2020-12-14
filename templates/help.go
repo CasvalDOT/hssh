76461d3bb23c4e28b119f63ef6dd9f443a0365aa
Casval <fabrizio.gortani@gmail.com>
2020-12-14 20:16:11 +0100

    docs: update help template

--- templates/help.go
+++ templates/help.go
@@ -15,6 +15,7 @@ hssh <options>
 -f enable fuzzysearch
 -e search through entire list of connections and execute the ssh command
 -s sync connections file from the repository
+-c enable colors
 -C create a new exemple configuration file. (this will not overwrite your actual conf)
 
 ## CUSTOM ALIAS
