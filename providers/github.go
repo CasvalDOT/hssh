651dc8bd98e81e3d32a5d92b93f9cb4a48065d73
Casval <fabrizio.gortani@protonmail.com>
2021-01-17 16:24:17 +0100

    fix: add missing control over http response

--- providers/github.go
+++ providers/github.go
@@ -3,6 +3,7 @@ package providers
 import (
 	"encoding/base64"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -49,6 +50,10 @@ func (g *github) get(endpoint string, queryParams []queryParam) ([]byte, error) 
 	body, err := ioutil.ReadAll(response.Body)
 	if err != nil {
 		return nil, err
+	}
+
+	if response.StatusCode < 200 || response.StatusCode > 299 {
+		return nil, errors.New(string(body))
 	}
 
 	return body, nil
