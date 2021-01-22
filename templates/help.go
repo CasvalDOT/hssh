109ec2b90372e75addc2736e3fd4203ed1288d34
Casval <fabrizio.gortani@protonmail.com>
2021-01-22 19:37:52 +0100

    refactor: remove -e flag

--- templates/help.go
+++ templates/help.go
@@ -13,7 +13,6 @@ hssh <options>
 -h show this message
 -l return all ssh connections
 -f enable fuzzysearch
--e search through entire list of connections and execute the ssh command
 -s sync connections file from the repository
 -c enable colors
 -C create a new exemple configuration file. (this will not overwrite your actual conf)
